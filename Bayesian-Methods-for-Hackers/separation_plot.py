840cd9e03f84bcf86d45d9cdb23bf1ed0b4344db
James Le <le_j6@denison.edu>
2019-07-10 08:42:22 -0700

    Chapter 2 A Little More on PyMC

---
+++ Bayesian-Methods-for-Hackers/separation_plot.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def separation_plot( p, y, **kwargs ):
+    """
+    This function creates a separation plot for logistic and probit classification.
+    See http://mdwardlab.com/sites/default/files/GreenhillWardSacks.pdf
+
+    p: The proportions/probabilities, can be a nxM matrix which represents M models.
+    y: the 0-1 response variables.
+
+    """
+    assert p.shape[0] == y.shape[0], "p.shape[0] != y.shape[0]"
+    n = p.shape[0]
+
+    try:
+        M = p.shape[1]
+    except:
+        p = p.reshape( n, 1 )
+        M = p.shape[1]
+
+    colors_bmh = np.array( ["#eeeeee", "#348ABD"] )
+
+
+    fig = plt.figure( )
+
+    for i in range(M):
+        ax = fig.add_subplot(M, 1, i+1)
+        ix = np.argsort( p[:,i] )
+        #plot the different bars
+        bars = ax.bar( np.arange(n), np.ones(n), width=1.,
+                color = colors_bmh[ y[ix].astype(int) ],
+                edgecolor = 'none')
+        ax.plot( np.arange(n+1), np.append(p[ix,i], p[ix,i][-1]), "k",
+                 linewidth = 1.,drawstyle="steps-post" )
+        #create expected value bar.
+        ax.vlines( [(1-p[ix,i]).sum()], [0], [1] )
+        plt.xlim( 0, n)
+
+    plt.tight_layout()
+
+    return
